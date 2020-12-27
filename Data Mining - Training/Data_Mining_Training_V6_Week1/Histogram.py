639d64bea049db57a25163b00ca8d1abc6e4179b
Nguyen379 <anhnguyenle379@gmail.com>
2020-12-27 15:03:21 +0700

    Data Mining update

--- Data Mining - Training/Data_Mining_Training_V6_Week1/Histogram.py
+++ Data Mining - Training/Data_Mining_Training_V6_Week1/Histogram.py
@@ -8,6 +8,7 @@ ages = data['Age']
 bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 plt.hist(ages, bins=bins, edgecolor='black', log=True)
+# plt.yscale("log") # two methods to use log data
 # count all occurrences of each age, divided into columns of bins
 # plotting data in y axis in log help ...
 median_age = 29
---
+++ Data Mining - Training/Data_Mining_Training_V6_Week1/Testing.py
@@ -0,0 +1,4 @@
+from itertools import count
+index = count()
+next(index)
+print(index)
